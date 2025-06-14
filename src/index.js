a389937b52481b86d2a690fbab1c89118a9b87a5
Abhishek Raj <abhishekraj995555@gmail.com>
2025-06-14 13:45:53 +0530

    Setup API Response and Error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import {app} from "./app.js"
+
 
 
 dotenv.config({
@@ -7,6 +9,15 @@ dotenv.config({
 })
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is Running at Port : ${process.env.PORT}`);
+    })
+})
+.catch(err => {
+    console.log("MongoDB Connection Failed !!!", err);
+    
+})
 
 
 
